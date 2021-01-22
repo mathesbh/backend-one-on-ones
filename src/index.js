c98f4088070362193bf9e3404e4463d42e4988e4
mathesbh <mathesbh@gmail.com>
2021-01-21 21:44:38 -0300

    Including and configuring cors

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-const PORT = 3000;
+const PORT = 3001;
 
 const express = require('express');
+const cors = require('cors')
 
 const app = express();
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
