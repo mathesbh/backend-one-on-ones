0303330c4edccca6b8704ca5035c4aa77ece2f24
Matheus <mathesbh@gmail.com>
2021-02-21 13:43:50 -0300

    Including controller to list users

---
+++ src/app/controllers/user.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const authMiddleware = require('../middlewares/auth');
+const User = require('../models/User');
+
+const router = express.Router();
+
+router.use(authMiddleware);
+
+router.get('/', async(req, res) => {
+  try{
+    const users = await User.find();
+
+    return res.status(200).send({ users });
+  }catch(err){
+    return res.status(400).send({ erro: 'Erro ao encontrar usuários' });
+  }
+});
+
+module.exports = app => app.use('/users', router);
