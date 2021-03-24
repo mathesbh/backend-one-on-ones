6cd80610dceef3e5424b186f2319aa4913c88d6b
mathesbh <mathesbh@gmail.com>
2021-03-24 18:47:15 -0300

    Create routes to find, change and delete meetings

--- src/app/controllers/meetingController.js
+++ src/app/controllers/meetingController.js
@@ -17,14 +17,46 @@ router.get('/', async(req, res) => {
     }
 });
 
+router.get('/:meetingId', async(req, res) => {
+    try{
+        const meeting = await Meeting.findById(req.params.meetingId);
+        
+        return res.status(200).send({ meeting });
+
+    }catch(err){
+        return res.status(404).send({ erro: 'Não foi possível listar encontro!' });
+    }
+});
+
 router.post('/', async (req, res) => {
     try {
-        const meeting = await Meeting.create({ ...req.body, user: req.userId });
+        const meeting = await Meeting.create({ ...req.body, assignedTo: req.userId });
 
         return res.status(201).send({ meeting });
 
     } catch(err){
         return res.status(400).send({ erro: 'Não foi possível criar encontro!' });
+    }
+});
+
+router.put('/:meetingId', async(req, res) => {
+    try{
+        const meeting = await Meeting.findByIdAndUpdate(req.params.meetingId, req.body, { new: true });
+        
+        return res.status(200).send({ meeting });
+    }catch(err){
+        return res.status(404).send({ erro: 'Não foi possível alterar o encontro!' });
+    }
+});
+
+router.delete('/:meetingId', async(req, res) => {
+    try{
+        const meeting = await Meeting.findByIdAndDelete(req.params.meetingId);
+        
+        return meeting === null ? res.status(204).send({ erro: 'Não foi possível deletar o encontro'}) : res.status(200).send({ info: 'Encontro removido com sucesso!' });
+        
+    }catch(err){
+        return res.status(404).send({ erro: 'Não foi possível listar o encontro!' });
     }
 });
 
